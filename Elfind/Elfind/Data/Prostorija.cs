b4e5a5eb2667451924273cc5ffe4a3e6f2c2feb8
Andjelija <mijajlovic.andjelija11@gmail.com>
2023-05-04 22:29:07 +0200

    Pomozi Boze

--- Elfind/Elfind/Data/Prostorija.cs
+++ Elfind/Elfind/Data/Prostorija.cs
@@ -4,7 +4,17 @@
     {
         public int ID { get; set; } 
 
-        protected string oznaka { get; set; }
+        public string Oznaka { get; set; }
+        public int Sprat { get; set; }
+        public float DownRightX { get; set; }
+        public float DownRightY { get; set; }
+        public float leftUpX { get; set; }
+        public float leftUpY { get; set; }
+        public int Kapacitet { get; set; }
+        public string TipProstorije { get; set; }
+        public Zgrada PripadaZgradi { get; set; }
+        public List <Cas> Casovi = new List <Cas> ();
+
 
 
     }
