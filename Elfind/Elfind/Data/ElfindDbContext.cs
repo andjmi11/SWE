b98bbb35ae8470395a3d95733de03cce5d149930
Andjelija <mijajlovic.andjelija11@gmail.com>
2023-05-08 15:00:49 +0200

    C:/Program Files/Git/

--- Elfind/Elfind/Data/ElfindDbContext.cs
+++ Elfind/Elfind/Data/ElfindDbContext.cs
@@ -12,23 +12,23 @@ namespace Elfind.Data
         {
 
         }
-        public DbSet<Prostorija> Prostorije { get; set; }
-        public DbSet<Zgrada> Zgrade { get; set; }
-        public DbSet<Student> Studenti { get; set; }
-        public DbSet<Smer> Smerovi { get; set; }
-        public DbSet<RasporedCasova> ResporediCasova { get; set; }
-        public DbSet<Objava> Opcija { get; set; }
-        public DbSet<Notifikacija> Notifikacije { get; set; }
-        public DbSet<NastavnoOsoblje> NastavnoOsoblje { get; set; }
+        public DbSet<Administrator> Administratori { get; set; }
+        public DbSet<Cas> Casovi { get; set; }
+        public DbSet<Forum> Forum { get; set; }
         public DbSet<Kurs> Kursevi { get; set; }
+        public DbSet<KursSmer> KursSmerSpoj { get; set; }
+        public DbSet<NastavnoOsoblje> NastavnoOsoblje { get; set; }
+        public DbSet<Notifikacija> Notifikacije { get; set; }
+        public DbSet<Objava> Objava { get; set; }
+        public DbSet<Opcija> Opcije { get; set; }
         public DbSet<OsobljeKurs> OsobljeKursSpoj { get; set; }
         public DbSet<OsobljeRaspored> OsobljeRasporedSpoj { get; set; }
+        public DbSet<Prostorija> Prostorije { get; set; }
+        public DbSet<RasporedCasova> ResporediCasova { get; set; }
+        public DbSet<Smer> Smerovi { get; set; }
+        public DbSet<Student> Studenti { get; set; }
         public DbSet<StudentKurs> StudentKursSpoj { get; set; }
-        public DbSet<KursSmer> KursSmerSpoj { get; set; }
-        public DbSet<Forum> Forum { get; set; }
-        public DbSet<Cas> Casovi { get; set; }
-        public DbSet<Administrator> Administratori { get; set; }
-        public object Objave { get; internal set; } //what
+        public DbSet<Zgrada> Zgrade { get; set; }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder options)
         //        => options.UseSqlServer("Default");
