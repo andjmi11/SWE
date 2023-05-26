a45dfe484031a6bb7d9ff6db3e4c0a8e48422d88
Kristina Joksimovic <joksimovic.kristina@elfak.rs>
2023-05-27 01:36:39 +0200

    sidebar

---
+++ Elfind/Elfind/Pages/Glavna.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elfind.Pages
+{
+    public class GlavnaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
